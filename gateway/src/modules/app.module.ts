1db87ef14cfea1a756c53e7edef9eab6ff1a6844
furkansahinfs <furkansahin_fs@outlook.com>
2024-01-23 16:46:31 +0300

    feat: provided throttler for rate limiting

--- gateway/src/modules/app.module.ts
+++ gateway/src/modules/app.module.ts
@@ -2,11 +2,12 @@ import { MiddlewareConsumer, Module } from "@nestjs/common";
 import * as path from "path";
 import { AcceptLanguageResolver, I18nModule } from "nestjs-i18n";
 import { JWTMiddleware, ResponseStatusInterceptor } from "src/middleware";
-import { APP_INTERCEPTOR } from "@nestjs/core";
+import { APP_GUARD, APP_INTERCEPTOR } from "@nestjs/core";
 import { AuthModule, CTModule } from "./subModules";
 import { AuthService } from "src/services";
 import { HttpModule } from "@nestjs/axios";
 import { ClientsModule } from "@nestjs/microservices";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 
 @Module({
   imports: [
@@ -18,6 +19,12 @@ import { ClientsModule } from "@nestjs/microservices";
       },
       resolvers: [AcceptLanguageResolver],
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     HttpModule,
     ClientsModule,
     AuthModule,
@@ -29,6 +36,10 @@ import { ClientsModule } from "@nestjs/microservices";
     {
       provide: APP_INTERCEPTOR,
       useClass: ResponseStatusInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
